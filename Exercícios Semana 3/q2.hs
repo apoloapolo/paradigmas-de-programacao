42ecbb1a033e0d8bb5b3e8a9613d288898010602
Vinicius Albuquerque (Apolo) <62688708+apoloapolo@users.noreply.github.com>
2022-07-13 19:42:58 -0300

    Terminada Questão 2 dos Exercícios da Semana 3

--- Exercícios Semana 3/q2.hs
+++ Exercícios Semana 3/q2.hs
@@ -14,4 +14,18 @@ type Telefone = Int
 type Pessoa = (Nome, Idade , Telefone)
 
 nometelefone :: Pessoa -> Pessoa -> Pessoa -> Pessoa -> String
-nometelefone (n1 , i1 , t1) (n2 , i2 , t2) (n3 , i3 , t3) (n4 , i4 , t4) = "dsds"
+nometelefone (n1 , i1 , t1) (n2 , i2 , t2) (n3 , i3 , t3) (n4 , i4 , t4) = verificapessoa (n1 , i1 , t1) s ++ verificapessoa (n2 , i2 , t2) s ++ verificapessoa (n3 , i3 , t3) s ++ verificapessoa (n4 , i4 , t4) s
+        where s = fromIntegral (i1 + i2 + i3 + i4) :: Float
+
+verificapessoa :: Pessoa -> Float -> String
+verificapessoa (n , i , t) s | idade (n , i , t) >= (s/4) = "| Nome: " ++ nome (n , i , t) ++ ", " ++ "Telefone: "++ telefone (n , i , t) ++ " |" | otherwise = ""
+
+nome :: Pessoa -> String
+nome (n , i , t) = n
+
+idade :: Pessoa -> Float
+idade (n , i , t) = id
+        where id = fromIntegral i :: Float
+
+telefone :: Pessoa -> String
+telefone (n , i , t) = show t
